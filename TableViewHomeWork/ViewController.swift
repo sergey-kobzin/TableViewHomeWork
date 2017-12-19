cc7c314264d476fed4b733a7bd83ccf332450eb8
Sergey Kobzin <kobzin@mac-mini-sergey.local>
2017-12-19 15:13:26 +0200

    Initial Commit

---
+++ TableViewHomeWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewHomeWork
+//
+//  Created by Sergey Kobzin on 19.12.2017.
+//  Copyright © 2017 Sergey Kobzin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
